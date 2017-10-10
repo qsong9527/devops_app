bb1bbf0a9f379e183b027eae5f4cdcfc32ff926e
Junyi Song <qsong@thoughtworks.com>
2017-10-10 15:06:29 +0800

    [App][Junyi]Add Maven support

---
+++ src/test/org/garrett/web/HelloWebTest.java
@@ -0,0 +1,14 @@
+package org.garrett.web;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class HelloWebTest {
+
+    @Test
+    public void testAssertEqual() throws Exception {
+        assertEquals(250, 250);
+    }
+
+}
